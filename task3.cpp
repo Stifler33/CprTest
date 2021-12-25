5628dd6d66e80743e7b13b5a239cd722fa8ff0dd
stifler33 <bagi33.ru@gmail.com>
2021-12-25 21:07:54 +0300

    task3 checkout

--- task3.cpp
+++ task3.cpp
@@ -8,7 +8,7 @@ int main(){
     string input;
     int countElement = 0;
     do{
-        cout << "Enter element " << countElement << endl;
+        cout << "Enter element " << countElement + 1 << endl;
         request.emplace_back(vector<string>(2));
         for (int i = 0; i < 2; i++){
             if (request.back()[0] == "post" || request.back()[0] == "get") continue;
@@ -16,5 +16,21 @@ int main(){
         }
         countElement++;
     }while((request.back()[0] != "get") && (request.back()[0] != "post"));
+
+    if (request.back()[0] == "get"){
+        string ReqUrl = "http://httpbin.org/get?";
+        auto& ReqEnd = request.back();
+        vector<vector<string>>::iterator itReq = request.end();
+        itReq -= 2;
+        for (auto& VecReq : request){
+            if (VecReq != request.back()) {
+                ReqUrl += VecReq[0] + '=' + VecReq[1];
+            }
+        }
+        cout << ReqUrl;
+    }else if (request.back()[0] == "post"){
+
+    }
+
     return 0;
 }
